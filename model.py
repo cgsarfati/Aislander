764f858ecea284bf62f50aafc003d215541fc480
vagrant <vagrant@vagrant.vm>
2017-08-07 23:21:32 +0000

    finished helper functions

--- model.py
+++ model.py
@@ -249,3 +249,20 @@ class RecipeCategory(db.Model):
 
 #####################################################################
 # Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///groceries'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # Ability to play with table definitions in interactive mode
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
