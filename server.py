8ff77f5a08137097df096e89bb71ef144da60eb3
vagrant <vagrant@vagrant.vm>
2017-08-07 23:37:58 +0000

    set up server.py so model file can run interactively

---
+++ server.py
@@ -0,0 +1,30 @@
+""" Grocery List App. """
+
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db, User, Recipe, Ingredient
+from model import List, CategoryRecipe, CategoryIngredient, RecipeIngredient
+from model import ListIngredient, Bookmark, RecipeCategory
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# So that undefined variables in Jinja2 will strike an error vs. failing silently
+app.jinja_env.undefined = StrictUndefined
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
